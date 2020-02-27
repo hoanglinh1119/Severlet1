19ceb9873035c279d3ebcd520bcc33cd8fa0b708
hoanglinh1119 <57972639+hoanglinh1119@users.noreply.github.com>
2020-02-27 23:49:16 +0700

    len

--- chuyen-doi-tien-te/src/ConvertServlet.java
+++ chuyen-doi-tien-te/src/ConvertServlet.java
@@ -4,19 +4,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "Chuyen-doi-tien-te",urlPatterns = "/convert")
-public class Chuyendoitiente extends HttpServlet {
+@WebServlet(name = "Convert-Servlet")
+public class ConvertServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     float rate= Float.parseFloat(request.getParameter("rate"));
-     float usd=Float.parseFloat(request.getParameter("usd"));
-     float vnd=rate*usd;
-        PrintWriter writer=response.getWriter();
-        writer.println(" Curency-Conventer");
-        writer.println("Rate: "+rate);
-        writer.println("Usd :"+usd);
-        writer.println("Vnd :"+vnd);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- chuyen-doi-tien-te/web/index.jsp
+++ chuyen-doi-tien-te/web/index.jsp
@@ -9,8 +9,19 @@
 <html>
   <head>
     <title>$Title$</title>
+    <link rel="stylesheet" href="index.css">
   </head>
   <body>
-  $END$
+    <div class="all">
+      <h2 class="head"> Curency conventer</h2>
+      <form class="form" method="post" action="convert">
+        <ul>
+          <li><a>Rate :</a> <input type="text"  name="rate" value="22000" placeholder="Rate"></li>
+          <li><a>USD :</a> <input name="usd" type="text" value="1" placeholder="usd"></li>
+          <li> <input type="submit" value="coventer" id="submit"></li>
+        </ul>
+      </form>
+
+    </div>
   </body>
 </html>
